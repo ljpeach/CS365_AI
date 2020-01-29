fb6e4c24256660b11a1ff1839984d775eeb85c6f
RogerLu <ljctbs520@gmail.com>
2020-01-29 01:29:44 -0500

    bug fix

--- lab1/maze_initializer.py
+++ lab1/maze_initializer.py
@@ -12,24 +12,23 @@ def maze_initializer(file):
 
 	prize_position = list()
     # a list record the position of the prizes
-
 	for i in file_contents:
 		maze_array.append(list(i.strip()))
 	# 2D array generated
 
-    # code to print the maze for easier visualization
+
+	# code to print the maze for easier visualization
     #for i in maze_array:
 	#	for j in i:
 	#		print(j, end = "")
 	#	print()
 
 	for i in range(len(maze_array)):
-    # iterate through the array to get the position of the mouse, append the prize position
-
-        for j in range(len(maze_array[i])):
-		    if maze_array[i][j] == "P":
-			    mouse_position = (i,j)
-		    if maze_array[i][j] == ".":
-			    prize_position.append((i,j))
-
+	# iterate through the array to get the position of the mouse, append the prize position
+		for j in range(len(maze_array[i])):
+			if maze_array[i][j] == "P":
+				mouse_position = (i,j)
+			if maze_array[i][j] == ".":
+				prize_position.append((i,j))
+                
 	return (mouse_position, prize_position, maze_array)
